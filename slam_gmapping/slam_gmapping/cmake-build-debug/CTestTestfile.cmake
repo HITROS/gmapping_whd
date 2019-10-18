53a780af3c2f1e37711888529dbcf0c6ff49ba9c
Karda-Jhin <984682920@qq.com>
2019-10-18 23:58:28 +0800

    1

---
+++ slam_gmapping/slam_gmapping/cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/sparta/catkin_slam/src/slam_gmapping/slam_gmapping
+# Build directory: /home/sparta/catkin_slam/src/slam_gmapping/slam_gmapping/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
